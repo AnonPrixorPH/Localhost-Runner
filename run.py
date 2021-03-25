71f810981fe9d2092f08c9c0c68871188c9698d1
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2021-03-26 05:46:32 +0800

    Localhost Runner | Termux
    Localhost Runner | Termux

---
+++ run.py
@@ -0,0 +1,8 @@
+# Localhost Runner
+import os
+import time
+import sys
+print("Welcome to Localhost Runner")
+print("Created By AnonPrixor")
+port = input("Enter Port: ")
+os.system("php -S localhost:" + port)
